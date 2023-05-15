7bced1355e2a92a08f1e13895416d13447a7fd6a
bmw <fuo_18@acelys.fr>
2023-05-15 10:57:06 +0200

    front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {VenteComponent} from "./vente/vente.component";
+import {ShopComponent} from "./shop/shop.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'vente', component: VenteComponent },
+  { path: 'shop', component: ShopComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { VenteComponent } from './vente/vente.component';
+import { ShopComponent } from './shop/shop.component';
+import { FootballComponent } from './football/football.component';
+import { HandballComponent } from './handball/handball.component';
+import { BasketballComponent } from './basketball/basketball.component';
+import { DetailComponent } from './detail/detail.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CartComponent } from './cart/cart.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    VenteComponent,
+    ShopComponent,
+    FootballComponent,
+    HandballComponent,
+    BasketballComponent,
+    DetailComponent,
+    CheckoutComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
